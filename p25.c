80df0a918263df8cb166bf0c2ede86a6347dd91f
Kevin <kevinkim1991@gmail.com>
2015-03-02 23:24:13 -0800

    solved

--- p25.c
+++ p25.c
@@ -1,12 +1,24 @@
-/* Problem 22
+/* Problem 25
  *
  * Get the first 1000th digit Fibonacci number
  *
  */
 #include <stdio.h>
+#include <stdlib.h>
+#include "bignumber.h"
 
 int main(){
-    int first_term = 1;
-    int second_term = 1;
-    while (second
+    num *first_term = init_num("1", 1);
+    num *second_term = init_num("1", 1);
+    num *a, *b;
+    int i = 2;
+    while (second_term->d < 1000){
+        a = first_term;
+        b = second_term;
+        second_term = add(second_term, first_term);
+        first_term = b;
+        free(a);
+        i++;
+    }
+    printf("%d\n", i);
 }
