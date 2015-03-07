030980980a376a847120df60d81ee088c715d179
Kevin <kevinkim1991@gmail.com>
2015-03-07 10:17:05 -0800

    Update

---
+++ p030.c
@@ -0,0 +1,23 @@
+/* There are only three numbers that can be written as the sum of fourth powers of their digits"
+ *
+ * 1634 = 1^4 + 6^4 + 3^4 + 4^4
+ * 8208 = 8^4 + 2^4 + 0^4 + 8^4
+ * 9474 = ...
+ *
+ * As 1 = 1^4 is not a sum is it not included.
+ * The sum of 1634 + 8208 + 9474 = 19316.
+ * Find the sum of all numbers that can be written as the sum of fifth powers of their digits.
+ *
+ * Note: We must determine the upper bound of such number.
+ *       Observe x9^5 must be an x digit number.
+ *
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    return 0;
+}
+
